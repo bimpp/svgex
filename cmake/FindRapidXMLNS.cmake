a5adcdc5ea3dbec5a4d77733d0f962d74c3797b1
Xing Ji <jixingcn@gmail.com>
2020-09-15 23:38:22 +0800

    just test

---
+++ cmake/FindRapidXMLNS.cmake
@@ -0,0 +1,9 @@
+set(RAPIDXMLNS_PATH "${BIMSVG_PATH_EXTERNALS}/rapidxml_ns")
+
+find_path(RAPIDXMLNS_PATH_INCLUDE "rapidxml_ns.hpp" "${RAPIDXMLNS_PATH}")
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(RAPIDXMLNS DEFAULT_MSG RAPIDXMLNS_PATH_INCLUDE)
+
+mark_as_advanced(RAPIDXMLNS_PATH_INCLUDE)
